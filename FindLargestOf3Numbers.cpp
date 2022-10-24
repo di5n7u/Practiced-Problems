780788117539a6f0b85b47c65fd17ee8a0f8b932
di5n7u <116501152+di5n7u@users.noreply.github.com>
2022-10-24 20:09:36 +0530

    Find Largest of 3 Numbers
    Input three numbers, print the largest of these numbers
    
    Input Format
    Three numbers (both positive and negative numbers are allowed)
    
    Output Format
    the largest number
    
    Sample Input
    5 7 4
    Sample Output
    7

---
+++ FindLargestOf3Numbers.cpp
@@ -0,0 +1,18 @@
+// Input three numbers, print the largest of these numbers
+#include<iostream>
+#include <climits>// To get least value of buckets
+using namespace std;
+int main() {
+	int i=1;
+	int no;
+	int largest=INT_MIN; // INT_MIN== -2^(31)
+	while (i<=3){
+		cin>>no;
+		if(largest<no){
+			largest=no;
+		}
+		i=i+1;
+	}
+	cout<<largest<<endl;
+	return 0;
+}

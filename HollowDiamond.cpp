45588b97c3e0e6b8347aab731cb8948bd6d6a80f
di5n7u <di5n7u@gmail.com>
2022-10-25 20:55:04 +0530

    Hollow Diamond

--- HollowDiamond.cpp
+++ HollowDiamond.cpp
@@ -1,4 +1,33 @@
-//Hollow Diamond
+/*
+Problem
+
+Take N (number of rows), print the following pattern (for N = 5).
+
+     * * * * *
+     * *   * *
+     *       *
+     * *   * *
+     * * * * *
+Input Format
+Constraints
+0 < N < 10 (where N is an odd number)
+
+Output Format
+Sample Input
+5
+Sample Output
+*	*	*       *      *		  
+*	*	        *      *
+* 	                   *
+* 	*           *      *
+* 	*	*       *      *
+	
+Explanation
+Each '*' is separated from other by a tab.
+*/
+
+//Solution
+
 #include <iostream>
 using namespace std;
 int main(){

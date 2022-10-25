9fdd9a53b8ec5f8e4ca3702550ed5be51e558f58
di5n7u <di5n7u@gmail.com>
2022-10-25 12:18:45 +0530

    Save 2_QuadraticEquation

---
+++ 2_QuadraticEquation.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+	int a,b,c,d,r1,r2;
+	cin>>a>>b>>c;
+	d=b*b-4*a*c;
+	if(d<0){
+		cout<<"Imaginary"<<endl;
+	}
+	else if(d>=0){
+		r1=(-b+sqrt(d))/2*a;
+		r2=(-b-sqrt(d))/2*a;
+		if(r1<r2){
+			cout<<"Real and Distinct"<<endl;
+			cout<<r1<<' '<<r2;
+		}
+		else if(r1>r2){
+			cout<<"Real and Distinct"<<endl;
+			cout<<r2<<' '<<r1;
+		}
+		else{
+			cout<<"Real and Equal"<<endl;
+			cout<<r1<<' '<<r2;
+		}
+	}
+	cout<<endl;
+	return 0;
+}

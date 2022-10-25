7802a53ca2071f6584aeb2cb0cc05b997f65cf88
di5n7u <di5n7u@gmail.com>
2022-10-25 22:27:14 +0530

    Pattern Mountain

--- MountainPattern.cpp
+++ MountainPattern.cpp
@@ -1,4 +1,29 @@
-//Pattern Mountain
+/*
+Problem
+
+Take N (number of rows), print the following pattern (for N = 4).
+
+                       1           1
+                       1 2       2 1  
+                       1 2 3   3 2 1
+                       1 2 3 4 3 2 1   
+Input Format
+Constraints
+0 < N < 10
+
+Output Format
+Sample Input
+4
+Sample Output
+1						1
+1	2				2	1
+1	2	3		3	2	1
+1	2	3	4	3	2	1
+
+*/
+
+//Solution
+
 #include <iostream>
 using namespace std;
 int main(){

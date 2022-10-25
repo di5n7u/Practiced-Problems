dbc764694370c77b3326fd7511e40ef78acf5c52
di5n7u <di5n7u@gmail.com>
2022-10-25 22:33:19 +0530

    Pascal Triangle 1

--- PascalTriangle1.cpp
+++ PascalTriangle1.cpp
@@ -1,4 +1,31 @@
-//Pascal Triangle 1
+/*
+Problem
+
+Given an integer N, print Pascal Triangle upto N rows. 
+
+Input Format
+Single integer N.
+
+Constraints
+N <= 10
+
+Output Format
+Print pascal triangle.
+
+Sample Input
+4
+Sample Output
+       1
+     1   1
+   1   2   1
+ 1   3   3   1
+
+Explanation
+Last row has one " "
+*/
+
+//Solution
+
 #include <iostream>
 using namespace std;
 int main(){
